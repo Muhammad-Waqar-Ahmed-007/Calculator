dced332d82000da2906b2b7420acf123a76df5fa
talha <talha@SIDDIQUI>
2023-06-08 14:49:21 +0500

    Add project files.

---
+++ ConsoleApp1/Bitmap.cs
@@ -0,0 +1,11 @@
+﻿public class Bitmap
+{
+    private object width;
+    private object height;
+
+    public Bitmap(object width, object height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class ImageProcessor
+{
+    public static Bitmap ParallelImageProcessing(Bitmap inputImage)
+    {
+        Bitmap outputImage = new Bitmap(inputImage.Width, inputImage.Height);
+
+        // Lock the input and output images for direct memory access
+        BitmapData inputData = inputImage.LockBits(new Rectangle(0, 0, inputImage.Width, inputImage.Height),
+                                                  ImageLockMode.ReadOnly, inputImage.PixelFormat);
+        BitmapData outputData = outputImage.LockBits(new Rectangle(0, 0, outputImage.Width, outputImage.Height),
+                                                    ImageLockMode.WriteOnly, outputImage.PixelFormat);
+
+        try
+        {
+            // Get the number of available processors
+            int numProcessors = Environment.ProcessorCount;
+
+            // Divide the image into equal-sized chunks for parallel processing
+            int chunkHeight = inputImage.Height / numProcessors;
+
+            // Perform parallel processing on each chunk of the image
+            Parallel.For(0, numProcessors, processorIndex =>
+            {
+                // Calculate the starting and ending Y coordinates for this processor
+                int startY = processorIndex * chunkHeight;
+                int endY = startY + chunkHeight;
+
+                // Process each pixel in the chunk
+                for (int y = startY; y < endY; y++)
+                {
+                    for (int x = 0; x < inputImage.Width; x++)
+                    {
+                        // Perform image processing operations on the pixel
+                        Color inputPixel = GetPixel(inputData, x, y);
+                        Color outputPixel = ProcessPixel(inputPixel);
+
+                        // Set the processed pixel in the output image
+                        SetPixel(outputData, x, y, outputPixel);
+                    }
+                }
+            });
+        }
+        finally
+        {
+            // Unlock the input and output images
+            inputImage.UnlockBits(inputData);
+            outputImage.UnlockBits(outputData);
+        }
+
+        return outputImage;
+    }
+
+    // Helper method to get the color of a pixel from bitmap data
+    private static Color GetPixel(BitmapData bitmapData, int x, int y)
+    {
+        unsafe
+        {
+            byte* pixelPtr = (byte*)bitmapData.Scan0 + y * bitmapData.Stride + x * 4;
+            byte b = pixelPtr[0];
+            byte g = pixelPtr[1];
+            byte r = pixelPtr[2];
+            return Color.FromArgb(r, g, b);
+        }
+    }
+
+    // Helper method to set the color of a pixel in bitmap data
+    private static void SetPixel(BitmapData bitmapData, int x, int y, Color color)
+    {
+        unsafe
+        {
+            byte* pixelPtr = (byte*)bitmapData.Scan0 + y * bitmapData.Stride + x * 4;
+            pixelPtr[0] = color.B;
+            pixelPtr[1] = color.G;
+            pixelPtr[2] = color.R;
+            pixelPtr[3] = color.A;
+        }
+    }
+
+    // Example image processing operation
+    private static Color ProcessPixel(Color inputPixel)
+    {
+        // Perform some image processing operation on the pixel
+        // For example, invert the color
+        int r = 255 - inputPixel.R;
+        int g = 255 - inputPixel.G;
+        int b = 255 - inputPixel.B;
+        return Color.FromArgb(r, g, b);
+    }
+}
